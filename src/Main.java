96a86682ffb8a3b4893c127b9cd7f4140fb48747
TheGhost025 <mazenkaluoby111@gmail.com>
2022-12-20 17:33:23 +0200

    First Fit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         LinkedList<Process> processes=new LinkedList<Process>();
         int maxNumOfPartition;
         int maxNumOfProcess;
+        int index=0;
         Scanner myObj=new Scanner(System.in);
 
         System.out.print("Enter number of Partition ");
@@ -34,9 +35,96 @@ public class Main {
 
         //Fisrt Fit
         if(choose==1){
+                int sizePartiton=0;
+                for(int i=0;i<maxNumOfProcess;i++)
+                {
+                    for (int j = 0; j < partitions.size(); j++)
+                    {
+                        if ((partitions.get(j).getPartitionSize()>= processes.get(i).getProcessSize())&&(partitions.get(j).getPartitionProcess()==null))
+                        {
+                            index = j;
+                            sizePartiton=partitions.get(index).getPartitionSize()-processes.get(i).getProcessSize();
+                            partitions.get(index).setPartitionSize(sizePartiton);
+                            partitions.get(index).setPartitionProcess(processes.get(i));
+                            processes.get(i).setProcessState(true);
+                            partitions.get(index).setPartitionSize(processes.get(i).getProcessSize());
+                            if(sizePartiton!=0)
+                            {
+                                partitions.add(index+1,new Partition(sizePartiton,"Partition "+(partitions.size()+1)));
+                            }
 
+                            break;
+                        }
+                    }
+                }
+                for(int k=0;k<partitions.size();k++){
+                    System.out.print(partitions.get(k).getPartitionName()+" "+partitions.get(k).getPartitionSize()+ " KB => ");
+                    if(partitions.get(k).getPartitionProcess()!=null){
+                        System.out.print(partitions.get(k).getPartitionProcess().getProcessName());
+                    }
+                    else {
+                        System.out.print("External fragment");
+                    }
+                    System.out.println();
+                }
+                System.out.print("Do you want to compact? 1.yes 2.no ");
+                int state=myObj.nextInt();
+                //compaction
+                if(state==1)
+                {
+                    int summ=0;
+                    int sizee=partitions.size()+1;
+                    LinkedList<Partition> deletedPartion=new LinkedList<Partition>();
+                    for(int i=0;i<partitions.size();i++){
+                        if(partitions.get(i).getPartitionProcess()==null){
+                            summ+=partitions.get(i).getPartitionSize();
+                            deletedPartion.add(partitions.get(i));
+                        }
+                    }
+
+                    for(int i=0;i< deletedPartion.size();i++){
+                        partitions.remove(deletedPartion.get(i));
+                    }
+
+                    partitions.add(new Partition(summ,"Partition "+sizee));
+
+                    for(int i=0;i<maxNumOfProcess;i++)
+                    {
+                        if(!processes.get(i).getProcessState())
+                        {
+                            for(int j=0;j<partitions.size();j++)
+                            {
+                                if((partitions.get(j).getPartitionSize()>= processes.get(i).getProcessSize())&&(partitions.get(j).getPartitionProcess()==null))
+                                {
+                                    index=j;
+                                    sizePartiton=partitions.get(index).getPartitionSize()-processes.get(i).getProcessSize();
+                                    partitions.get(index).setPartitionSize(processes.get(i).getProcessSize());
+                                    partitions.get(index).setPartitionProcess(processes.get(i));
+                                    processes.get(i).setProcessState(true);
+                                    partitions.get(index).setPartitionSize(processes.get(i).getProcessSize());
+                                    if(sizePartiton!=0)
+                                    {
+                                        partitions.add(index+1,new Partition(sizePartiton,"Partition "+(sizee+1)));
+                                        sizee++;
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    for(int k=0;k<partitions.size();k++){
+                        System.out.print(partitions.get(k).getPartitionName()+" "+partitions.get(k).getPartitionSize()+ " KB => ");
+                        if(partitions.get(k).getPartitionProcess()!=null){
+                            System.out.print(partitions.get(k).getPartitionProcess().getProcessName());
+                        }
+                        else {
+                            System.out.print("External fragment");
+                        }
+                        System.out.println();
+                    }
+                }
         }
-        //Worst Fit
+    //Worst Fit
         else if(choose==2){
 
         }
@@ -44,7 +132,7 @@ public class Main {
         else if(choose==3){
             for(int i=0;i<maxNumOfProcess;i++){
                 int min=1000000000;
-                int index=0;
+                index=0;
                 for(int j=0;j<partitions.size();j++){
                     if((processes.get(i).getProcessSize()<=partitions.get(j).getPartitionSize())&&(partitions.get(j).getPartitionProcess()==null)){
                         if(min>=partitions.get(j).getPartitionSize()){
@@ -96,7 +184,7 @@ public class Main {
                 for(int i=0;i<maxNumOfProcess;i++){
                     if(!processes.get(i).getProcessState()){
                         int min=1000000000;
-                        int index=0;
+                        index=0;
                         for(int j=0;j<partitions.size();j++){
                             if((processes.get(i).getProcessSize()<=partitions.get(j).getPartitionSize())&&(partitions.get(j).getPartitionProcess()==null)){
                                 if(min>=partitions.get(j).getPartitionSize()){
@@ -113,6 +201,7 @@ public class Main {
                             if(sizeNewPartiton!=0){
                                 partitions.add(index+1,new Partition(sizeNewPartiton,"Partition "+(partitions.size()+1)));
                             }
+                            break;
                         }
                     }
                 }
@@ -129,5 +218,6 @@ public class Main {
                 }
             }
         }
+
     }
 }
